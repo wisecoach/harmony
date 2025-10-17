1f09bfc5a35d4d774e1f8a40b34c5017fa7e00b9
wisecoach <827165648@qq.com>
2025-10-17 16:09:14 +0800

    1. fix bug

--- test/test.sh
+++ test/test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-node ../cli/deployTCCTest.js
-cd ../cli-py
-python ../cli-py/simulate_tcc.py
+node ../ssc-cli/cli/deployTCCTest.js
+cd ../ssc-cli/cli-py
+python ./simulate_tcc.py
 cd -
 
 notify-send "TCC Test" "TCC test completed successfully."
